9db12cbc77567f5fb67023f3c9bb70f9e6c686c9
Fernando Ramirez <90127607+N4ndomx@users.noreply.github.com>
2024-09-05 03:13:39 -0600

    pag about

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { TopGamesComponent } from './top-games/top-games.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { FooterComponent } from './footer/footer.component';
 import { NgModule } from '@angular/core';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
     { path: 'topgames', component: TopGamesComponent },
     { path: '', component: LandingPageComponent },
-    { path: 'footer', component: FooterComponent }
+    { path: 'footer', component: FooterComponent },
+    { path: 'about', component: AboutComponent }
+
 ];
 
 @NgModule({
